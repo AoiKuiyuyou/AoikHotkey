48dd93cb44e322784cadfd4b62b27493bd6fe91e
Aoi.Kuiyuyou <aoi.kuiyuyou@gmail.com>
2015-03-25 16:52:59 +0800

    version 0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 setup(
     name='AoikHotkey',
 
-    version='0.1',
+    version='0.1.1',
 
     description="""AutoHotkey remake in Python. Hotkey calls Python function.""",
 
