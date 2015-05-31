57d42a341b816b265a3604d4e5d64d8436254dfa
Aoi.Kuiyuyou <aoi.kuiyuyou@gmail.com>
2015-05-31 22:19:47 +0800

    "hotkey_tfunc" prints hotkey definition

--- src/aoikhotkey/spec/tfunc.py
+++ src/aoikhotkey/spec/tfunc.py
@@ -18,10 +18,17 @@ def hotkey_tfunc(manager, hotkey, spec, type, event):
     else:
         up_dn_txt = ' '
 
+    #/ Spec format is determined at 7asgYVg, 6tC5Ktw and 9o2zOGe
+    spec_orig = spec[1]
+
     #/
-    msg = '#/ {type}{up_dn}triggered\n{virkey}\n\n'.format(
+    hotkey_def = spec_orig[0]
+
+    #/
+    msg = '#/ {type}{up_dn}triggered\n{hotkey_def}\n{virkey}\n\n'.format(
         type='Hotseq' if type == HOTKEY_TYPE_V_HS else 'Hotkey',
         up_dn=up_dn_txt,
+        hotkey_def=hotkey_def,
         virkey=' '.join(manager._vk_ctn(x) or str(x) for x in hotkey),
     )
 
